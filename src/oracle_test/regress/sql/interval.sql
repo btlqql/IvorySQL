65772275c233e5c4498fc418fe7fcad64ff8b4a7
btlqql <2977859784@qq.com>
2026-09-09 01:09:08 +0800

    test(interval): cover bare INTERVAL parser paths
    Signed-off-by: btlqql <2977859784@qq.com>

--- src/oracle_test/regress/sql/interval.sql
+++ src/oracle_test/regress/sql/interval.sql
@@ -805,4 +805,12 @@ reset ivorysql.enable_emptystring_to_null;
 SET ivorysql.compatible_mode = oracle;
 CREATE TABLE interval_bare_oracle (a interval);
 DROP TABLE interval_bare_oracle;
+-- Extension install scripts also reference a bare INTERVAL in CREATE CAST.
+CREATE CAST (int4 AS interval) WITH INOUT;
+DROP CAST (int4 AS interval);
+-- Function signatures in extension install scripts may use bare INTERVAL
+-- for both arguments and return type.
+CREATE FUNCTION interval_bare_oracle_func(interval) RETURNS interval LANGUAGE SQL AS $$ SELECT $1 $$;
+/
+DROP FUNCTION interval_bare_oracle_func(interval);
 SET ivorysql.compatible_mode = pg;
